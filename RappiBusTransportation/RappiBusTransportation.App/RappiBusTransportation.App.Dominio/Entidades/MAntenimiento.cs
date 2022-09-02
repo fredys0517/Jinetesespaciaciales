f9596dee26c499e4f1bf5be40bd0700b087e8b09
FredysSantos <Sfredys0517@hotmail.com>
2022-09-02 16:58:16 -0500

    organizacion clase

--- RappiBusTransportation/RappiBusTransportation.App/RappiBusTransportation.App.Dominio/Entidades/MAntenimiento.cs
+++ RappiBusTransportation/RappiBusTransportation.App/RappiBusTransportation.App.Dominio/Entidades/MAntenimiento.cs
@@ -6,10 +6,10 @@ public class Mantenimiento
     public string NivelLiquidoFreno {get;set;}
     public string NivelRefrigerante {get;set;}
     public string NivelLiquidoDireccion {get;set;}
-      public string IdVehiculos {get;set;}
-        public string IdMecanico {get;set;}
-          public string IdAuxiliar {get;set;}
-            public string FechaMantenimiento {get;set;}
+    public string IdVehiculos {get;set;}
+    public string IdMecanico {get;set;}
+    public string IdAuxiliar {get;set;}
+    public string FechaMantenimiento {get;set;}
 
 }
 
