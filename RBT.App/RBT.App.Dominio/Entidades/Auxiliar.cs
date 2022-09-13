a8a1671fb986438a24a00b63405438c038c5c97f
Julian Camacho <jd.camachoo29@gmail.com>
2022-09-13 09:43:40 -0500

    Auxiliar no podia ser detectado porque su namespace terminaba en dominio.entidades. Se borro la palabra entidades y ya funciona

--- RBT.App/RBT.App.Dominio/Entidades/Auxiliar.cs
+++ RBT.App/RBT.App.Dominio/Entidades/Auxiliar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RBT.App.Dominio.Entidades
+namespace RBT.App.Dominio
 {
     public class Auxiliar:Persona
     {
