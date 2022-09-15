0f5b750f2a234a7a7be6ddb2798f33d4939efad6
Julian Camacho <jd.camachoo29@gmail.com>
2022-09-15 10:10:22 -0500

    Creando la pagina log in

---
+++ RBT.WebApp/RBT.App.Frontend/Pages/LogIn/LogInMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RBT.App.Frontend.Pages
+{
+    public class LogInMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
