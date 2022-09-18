8c36259fc961e3fa234495d44cf2952c516c2721
Julian Camacho <jd.camachoo29@gmail.com>
2022-09-18 00:31:12 -0500

    Correcion de la palabra JefesOperaciones en plural

--- RBT.App/RBT.App.Persistencia/AppRepositorios/RepositorioJefeOperaciones.cs
+++ RBT.App/RBT.App.Persistencia/AppRepositorios/RepositorioJefeOperaciones.cs
@@ -17,51 +17,52 @@ namespace RBT.App.Persistencia
             _appContext=appContext;
         }
 
-        JefeOperaciones IRepositorioJefeOperaciones.AgregarJefeOperaciones(JefeOperaciones jefeOperaciones)
+        JefeOperaciones IRepositorioJefeOperaciones.AgregarJefeOperaciones(JefeOperaciones jefeoperaciones)
         {
-            var jefeOperacionesAgregado= _appContext.JefeOperacioness.Add(jefeOperaciones);
+            var jefeoperacionesAgregado= _appContext.JefesOperaciones.Add(jefeoperaciones);
             _appContext.SaveChanges();
-            return jefeOperacionesAgregado.Entity;
+            return jefeoperacionesAgregado.Entity;
         }
         
         void IRepositorioJefeOperaciones.EliminarJefeOperaciones(int idJefeOperaciones)
         {
-            var jefeOperacionesEncontrado = _appContext.JefeOperacioness.FirstOrDefault(m =>m.Id==idJefeOperaciones);
+            var jefeoperacionesEncontrado = _appContext.JefesOperaciones.FirstOrDefault(m =>m.Id==idJefeOperaciones);
             
-            if (jefeOperacionesEncontrado==null)
+            if (jefeoperacionesEncontrado==null)
                 return;
-            _appContext.JefeOperacioness.Remove(jefeOperacionesEncontrado);
+            _appContext.JefesOperaciones.Remove(jefeoperacionesEncontrado);
             _appContext.SaveChanges();
                 
         }
 
         IEnumerable<JefeOperaciones> IRepositorioJefeOperaciones.ListarJefeOperacionesAll()
         {
-            return _appContext.JefeOperacioness;
+            return _appContext.JefesOperaciones;
         }
 
         JefeOperaciones IRepositorioJefeOperaciones.ObtenerJefeOperaciones(int idJefeOperaciones)
         {
-            return _appContext.JefeOperacioness.FirstOrDefault(m =>m.Id==idJefeOperaciones);
+            return _appContext.JefesOperaciones.FirstOrDefault(m =>m.Id==idJefeOperaciones);
 
         }
 
-        JefeOperaciones IRepositorioJefeOperaciones.ActualizarJefeOperaciones(JefeOperaciones jefeOperaciones)
-        {// el simbolo "=>" quiere decir donde. La letra "m" hace referencia a la tabla mecanicos.
-            var jefeOperacionesEncontrado= _appContext.JefesOperaciones.FirstOrDefault(m =>m.Id==jefeOperaciones.Id); // No estoy seguro de que sea "JefeOperaciones.Id"
+        JefeOperaciones IRepositorioJefeOperaciones.ActualizarJefeOperaciones(JefeOperaciones jefeoperaciones)
+        {// el simbolo "=>" quiere decir donde. La letra "m" hace referencia a la tabla jefesoperaciones.
+            var jefeoperacionesEncontrado= _appContext.JefesOperaciones.FirstOrDefault(m =>m.Id==jefeoperaciones.Id); // No estoy seguro de que sea "JefeOperaciones.Id"
 
-            if(jefeOperacionesEncontrado!=null)
+            if(jefeoperacionesEncontrado!=null)
             {
-                jefeOperacionesEncontrado.Nombres=jefeOperaciones.Nombres;
-                jefeOperacionesEncontrado.Apellidos=jefeOperaciones.Apellidos;
-                jefeOperacionesEncontrado.Telefono=jefeOperaciones.Telefono;
-                jefeOperacionesEncontrado.FechaNacimiento=jefeOperaciones.FechaNacimiento;
+                jefeoperacionesEncontrado.Nombres=jefeoperaciones.Nombres;
+                jefeoperacionesEncontrado.Apellidos=jefeoperaciones.Apellidos;
+                jefeoperacionesEncontrado.Telefono=jefeoperaciones.Telefono;
+                jefeoperacionesEncontrado.FechaNacimiento=jefeoperaciones.FechaNacimiento;                
                 _appContext.SaveChanges();
                 
             }
 
-            return jefeOperacionesEncontrado;
+            return jefeoperacionesEncontrado;
+
         }
-    
+
     }
-}    
+}
