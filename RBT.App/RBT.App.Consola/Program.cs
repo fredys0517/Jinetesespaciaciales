1d18e1f8471dbbf6ec87f9bd14233b55b739a7c9
Julian Camacho <jd.camachoo29@gmail.com>
2022-09-18 11:31:29 -0500

    trabajando en la consola

--- RBT.App/RBT.App.Consola/Program.cs
+++ RBT.App/RBT.App.Consola/Program.cs
@@ -2,16 +2,20 @@
 using RBT.App.Dominio;
 using RBT.App.Persistencia;
 
-namespace RBT.App.Dominio
+namespace RBT.App.Consola
 {
     class Program
     {
+        private static IRepositorioAuxiliar _repositorioAuxiliar = new RepositorioAuxiliar(new Persistencia.AppContext());
         static void Main(String[] args)
         {
             Console.WriteLine("Hello, World Entity Framework!");
         }
     }
 
+    //private static void IRepositorioAuxiliar.AgregarAuxiliar(RBT.App.Dominio.Auxiliar auxiliar);
+
+    
 
 }
 
